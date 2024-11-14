63682df45ed637e34a0a23a7b0ec10195a16c2c3
TARUNADHIKARI2829 <adhikari2829@gmail.com>
2024-11-14 09:16:40 +0000

    changes add krdo

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountid = 2829
+let accountemail = "hahah@gmail.com"
+var accountpassword = "12345"
+accountcity = "jaisalmer"
+
+//accountid = 22 // not allowed to use change constant //
+  
+//javaascript me mostly let use kro var nhi //
+//kyki issue ata h in block scope and functional scope //
+
+
+
+accountemail = "hihihi@gmail.com"
+accountpassword = "123"
+accountcity = "jaipur"
+console.log(accountid);
+
+console.table( [accountid,  accountemail, accountpassword, accountcity])
