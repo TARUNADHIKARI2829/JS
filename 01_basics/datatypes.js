272f5bb1c765fa434fcae2f119678c71ac8ce054
TARUNADHIKARI2829 <adhikari2829@gmail.com>
2024-11-15 08:25:47 +0000

    datatypes and emca standards

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3) we are using js not browser//
+ 
+
+confirm
+console.log(4
+    +5
+);// code readability should be high
+
+console.log("tarun")
+
+let name="tarun"
+let age=18
+
+
+// number=>2 to power 53
+// bigint used for big big values
+// string=>""
+//boolean=> true/false
+//null=> standalone value
+//undefined=>
+    //symbol=>unique
+
+console.log(typeof null);  //undefined 
+console.log(typeof undefined);//object
